a81e8aa5184d9edc06a0d403dda29bb9c7424a53
Thomas <lakemonsters.software.team@gmail.com>
2019-02-18 15:48:39 -0800

    Half of very basic climb system
    TB

---
+++ src/main/java/frc/robot/commands/ClimberRaiseFrontCommand.java
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class ClimberRaiseFrontCommand extends Command {
+  public ClimberRaiseFrontCommand() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.climber.COMMAND_GROUP_STAGE = 2;
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.climber.raiseFrontClimber();
+    Robot.driveSubsystem.tankDrive(0.05, 0.05);
+    //What if this puts pressure on legs and they can't retract?
+    //Run back wheel at same time and transition from wall contact on back legs?
+    //This is likely the worst stage to be cancelled out of, as there is a higher likelyhood of falling at an angle, causing damage to the mast
+    //What can we do in an instant before the robot is disabled???
+    //The pneumatic switch would be good, allowing us to remain stable if the front wheel is fully supported on the ledge
+    //Can we have a separate processor on the robot to slowly descend the robot, but what if part of the robot is over the ledge and it tips
+    //I also believe using a separate board to control motors specifically to drive while disabled would likely be a violation, being unsafe
+    //Not only PID values need to be tuned, but the speeds of motors no doubt need some very accurate tuning to not be too fast and make the robot bounce
+    //Will we have enough time with the robot to test this to standards? Or at all?
+    //What if the battery is low at the end of the match, how do we protect against brownouts ruining the climb?
+    
+    //How much time do we have to save our robot before we lose control???
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return Robot.climber.raiseFrontClimberIsFinished();
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,116 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class Climber extends Subsystem {
+  // Put methods for controlling this subsystem
+  WPI_TalonSRX FRExtender;
+  WPI_TalonSRX FLExtender;
+  WPI_TalonSRX BExtender;
+  WPI_TalonSRX BackDriveMotor;
+
+  int EXTENDER_HEIGHT = 0;
+
+  //This int will be used to track what stage the climb was cancelled in, and what to do about that cancel
+  public int COMMAND_GROUP_STAGE = 0;
+
+  public Climber() {
+    FRExtender = new WPI_TalonSRX(0);
+    FLExtender = new WPI_TalonSRX(0);
+    BExtender = new WPI_TalonSRX(0);
+    BackDriveMotor = new WPI_TalonSRX(0);
+
+    FRExtender.config_kP(0, 0);
+    FRExtender.config_kI(0, 0);
+    FRExtender.config_kD(0, 0);
+    FRExtender.config_kF(0, 0);
+
+  }
+ 
+  public void lowerClimber(){
+    FRExtender.set(ControlMode.Position, EXTENDER_HEIGHT);
+    FLExtender.set(ControlMode.Position, EXTENDER_HEIGHT);
+    BExtender.set(ControlMode.Position, EXTENDER_HEIGHT);
+
+  }public boolean lowerClimberIsFinished(){
+    int rightValue = FRExtender.getSelectedSensorPosition(0);
+    int leftValue = FLExtender.getSelectedSensorPosition(0);
+    int backValue = BExtender.getSelectedSensorPosition(0);
+
+    if(rightValue == EXTENDER_HEIGHT && leftValue == EXTENDER_HEIGHT && backValue == EXTENDER_HEIGHT){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public void raiseFrontClimber(){
+    FRExtender.set(ControlMode.Position, 0);
+    FLExtender.set(ControlMode.Position, 0);
+  }
+
+  public boolean raiseFrontClimberIsFinished(){
+    int rightValue = FRExtender.getSelectedSensorPosition(0);
+    int leftValue = FLExtender.getSelectedSensorPosition(0);
+
+    if(rightValue == 0 && leftValue == 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public void raiseBackClimber(){
+    BExtender.set(ControlMode.Position, 0);
+  }
+
+  public boolean raiseBackClimberIsFinished(){
+    int backValue = BExtender.getSelectedSensorPosition(0);
+
+    if(backValue == 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public void drive(double driveSpeed){
+    BackDriveMotor.set(driveSpeed);
+  }
+
+  /*
+  * One of two options needs to be chosen.
+  * Either we use the wall stallout to see when we hit the wall
+  * Or we assume we always start directly against the wall, 
+  * And use a constant amount of encoder counts to proceed
+  */
+  public boolean driveHitWall(){
+    if( BackDriveMotor.getOutputCurrent() > 2.0) {
+      System.out.println("current > 2");
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
